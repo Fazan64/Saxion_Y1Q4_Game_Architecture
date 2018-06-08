d7c39f7753aa77e0e0dbbdf3c456b0dc19b4d395
Maks <max.maisak@gmail.com>
2018-06-08 21:17:36 +0200

    Extracted most of the game-specific stuff into MainLevel

--- sources/Game.cs
+++ sources/Game.cs
@@ -31,13 +31,6 @@ namespace Engine
         private readonly RenderingManager renderingManager;
 
         private bool isRunning = false;
-        private Ball ball;
-        private AiPaddle leftPaddle;
-        private AiPaddle rightPaddle; // just for demo sake: mind that also the controls are commented out now
-
-        private Image b;
-        private Image fontSheet;
-        private uint leftScore, rightScore;
 
         public Vector2 size  => new Vector2(ClientSize.Width, ClientSize.Height);
 
@@ -57,10 +50,7 @@ namespace Engine
             ClientSize = new System.Drawing.Size(640, 480);
             ResumeLayout();
 
-            fontSheet = Image.FromFile("assets/digits.png");
-            b = Image.FromFile("assets/booster.png");
-
-            AddGameObjects();
+            var level = new MainLevel();
 
             Show(); // make form visible
             Run();
@@ -116,104 +106,12 @@ namespace Engine
 
             renderingManager.Render(paint.Graphics);
 
-            // TODO Move out everything below
-
-            paint.Graphics.DrawImage(b, 320 - 16, 120 - 16);
-            DisplayScore(paint.Graphics);
-
             //Debug.WriteLine("paint");
         }
 
         private void UpdateFixedTimestep()
         {
             updateManager.Step();
-
-            // TODO Move out everything below
-
-            CheckPaddleHit();
-
-            // what am I doing here, forgot ;( 
-            if (ball.x < 320 + 16 && ball.x + 16 > 320 - 16 && ball.y < 120 + 16 && ball.y + 16 > 120 - 16)
-            {
-                ball.SetBoost(true); // for 60 frames.... how long is that
-            }
-
-            CheckScore();
-        }
-
-        private void AddGameObjects()
-        {
-            ball = new Ball("Ball");
-            ball.Add<ImageRenderer>().SetImage("assets/ball.png");
-            ball.Add<Rigidbody>();
-
-            leftPaddle = new AiPaddle("Left", 20, ball);
-            leftPaddle.Add<ImageRenderer>().SetImage("assets/paddle.png");
-
-            rightPaddle = new AiPaddle("Right", 639 - 20, ball);
-            rightPaddle.Add<ImageRenderer>().SetImage("assets/paddle.png");
-        }
-
-        private void CheckPaddleHit()
-        {
-            Rigidbody ballRb = ball.Get<Rigidbody>();
-            // check for collisions with both paddles or when behind it and resolve
-            if (ball.x < leftPaddle.x + 8 && ball.x + 16 > leftPaddle.x && ball.y < leftPaddle.y + 64 && ball.y + 16 > leftPaddle.y)
-            {
-                Console.WriteLine("Left Hit " + Math.Abs((leftPaddle.y - 32) - (ball.y + 8))); // just for testing
-                                                                                               // change y speed depending on hit offset in y
-                float dy = Math.Abs((leftPaddle.y - 32) - (ball.y + 8)) / 50.0f;
-
-                ball.Resolve(leftPaddle.x + 8, ball.y, +Math.Abs(ballRb.velocity.x), dy * ballRb.velocity.y); // hmm
-            }
-
-            if (ball.x < rightPaddle.x + 8 && ball.x + 16 > rightPaddle.x && ball.y < rightPaddle.y + 64 && ball.y + 16 > rightPaddle.y)
-            {
-                Console.WriteLine("Right Hit");
-                float dy = Math.Abs((rightPaddle.y - 32) - (ball.y + 8)) / 50.0f;
-                ball.Resolve(rightPaddle.x - 16, ball.y, -Math.Abs(ballRb.velocity.x), dy * ballRb.velocity.y); // awful, but who cares, no one is gonna see this.
-            }
-        }
-
-        private void CheckScore()
-        {
-            float x = ball.position.x;
-            float y = ball.y;
-
-            if (ball.position.x < 0f) // left score
-            {
-                rightScore++;
-                ball.Reset();
-            }
-            else if (ball.position.x > 639f - 16f) // right score
-            {
-                leftScore++;
-                ball.Reset();
-            }
-        }
-
-        private void DisplayScore(Graphics graphics)
-        {
-            int digits = 2;
-            int leftX = 240; // left score first
-            int y = 25;
-            string leftScore = "000" + this.leftScore.ToString(); // convert to string and add preceeding 000
-            for (int d = 0; d < digits; d++)
-            { // 3 digits left to right
-                int digit = leftScore[leftScore.Length - digits + d] - 48; // '0' => 0 etc
-                Rectangle rect = new Rectangle(digit * fontSheet.Width / 10, 0, fontSheet.Width / 10, fontSheet.Height);
-                graphics.DrawImage(fontSheet, leftX + d * fontSheet.Width / 10, y, rect, GraphicsUnit.Pixel);
-            }
-
-            // oh no, same thing for right score !!!
-            int rightX = 639 - 240 - digits * fontSheet.Width / 10;
-            string rightScore = "000" + this.rightScore.ToString(); // convert to string and add preceeding 000
-            for (int d = 0; d < digits; d++)
-            { // 3 digits left to right
-                int digit = rightScore[rightScore.Length - digits + d] - 48; // '0' => 0 etc
-                Rectangle rect = new Rectangle(digit * fontSheet.Width / 10, 0, fontSheet.Width / 10, fontSheet.Height);
-                graphics.DrawImage(fontSheet, rightX + d * fontSheet.Width / 10, y, rect, GraphicsUnit.Pixel);
-            }
         }
     }
 }
